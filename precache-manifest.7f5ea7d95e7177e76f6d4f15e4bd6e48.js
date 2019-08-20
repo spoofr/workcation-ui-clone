fb761c467d73e3819754f563fe42a591c5eaa487
Syd <sydmrmdn@gmail.com>
2019-08-20 11:37:02 +0800

    Deploy

---
+++ precache-manifest.7f5ea7d95e7177e76f6d4f15e4bd6e48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f5211fcb1c21874837",
+    "url": "/workcation-ui-clone/js/about.68142405.js"
+  },
+  {
+    "revision": "edc6d4adbb609c83659e",
+    "url": "/workcation-ui-clone/css/app.9f7b0d40.css"
+  },
+  {
+    "revision": "edc6d4adbb609c83659e",
+    "url": "/workcation-ui-clone/js/app.ea2990e9.js"
+  },
+  {
+    "revision": "92c8a67c96824705eba7",
+    "url": "/workcation-ui-clone/js/chunk-vendors.8fca2857.js"
+  },
+  {
+    "revision": "2354c59de6c059e269cef9814a02174a",
+    "url": "/workcation-ui-clone/img/cat.2354c59d.jpg"
+  },
+  {
+    "revision": "e17432d9192fc159f610ee2e95c22806",
+    "url": "/workcation-ui-clone/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/workcation-ui-clone/robots.txt"
+  }
+];
